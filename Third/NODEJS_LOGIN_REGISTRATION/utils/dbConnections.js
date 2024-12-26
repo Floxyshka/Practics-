10108759cbc32abe405e4930e44536719d07dd74
Floxyshka <ipocerfacermaster@gmail.com>
2024-12-26 15:41:57 +0500

    Update dbConnections.js

--- Third/NODEJS_LOGIN_REGISTRATION/utils/dbConnections.js
+++ Third/NODEJS_LOGIN_REGISTRATION/utils/dbConnections.js
@@ -1,10 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const dbConnection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodejs_login'
-});
+const db_connection = mysql
+    .createConnection({
+        host: "127.0.0.1", // имя хоста
+        user: "root", // имя пользователя
+        database: "MITE41_Mineev", // имя базы данных
+        password: "admin", // пароль от базы данных
+    })
+    .on("error", (err) => {
+        console.log("Failed to connect to Database - ", err);
+    });
 
-module.exports = dbConnection.promise();
+module.exports = db_connection;
